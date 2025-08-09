3df9d374f048152ea72ea8de8b09d3eb77cccd0c
Julius Kedienye <juliuskedienye61@gmail.com>
2025-08-09 23:01:31 +0300

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,51 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Unknown", breed="Pug"):
+        """
+        Initialize a Dog instance.
+        Default breed is 'Pug' so that when testing name errors,
+        the breed validation does not also fail and print extra output.
+        """
+        self.name = name   # Calls name setter
+        self.breed = breed # Calls breed setter
+
+    # -----------------
+    # name property
+    # -----------------
+    def get_name(self):
+        """Return dog's name."""
+        return self._name
+
+    def set_name(self, value):
+        """
+        Validate and set dog's name.
+        - Must be a string between 1 and 25 characters.
+        If invalid, print only the name error message.
+        """
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # -----------------
+    # breed property
+    # -----------------
+    def get_breed(self):
+        """Return dog's breed."""
+        return self._breed
+
+    def set_breed(self, value):
+        """
+        Validate and set dog's breed.
+        - Must be in the APPROVED_BREEDS list.
+        If invalid, print only the breed error message.
+        """
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# List of approved job titles
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -16,4 +17,52 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Unknown"):
+        """
+        Initialize a new Person instance.
+        Uses the property setters so that validation occurs automatically.
+        """
+        self.name = name  # Calls the name setter
+        self.job = job    # Calls the job setter
+
+    # -----------------
+    # name property
+    # -----------------
+    def get_name(self):
+        """Return the person's name."""
+        return self._name
+
+    def set_name(self, value):
+        """
+        Set the person's name.
+        - Must be a string.
+        - Must be between 1 and 25 characters.
+        - Will be converted to title case before storing.
+        If invalid, print an error message.
+        """
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()  # Convert to title case
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    # -----------------
+    # job property
+    # -----------------
+    def get_job(self):
+        """Return the person's job title."""
+        return self._job
+
+    def set_job(self, value):
+        """
+        Set the person's job title.
+        - Must be in the APPROVED_JOBS list.
+        If invalid, print an error message.
+        """
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
